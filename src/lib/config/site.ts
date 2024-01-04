2ce3e26b5d5a074acdd2ad779080c9e3bef13c9f
mbouchonnet <mathieubouchonnet@gmail.com>
2024-01-04 10:03:47 +0100

    try modif for rss and sitemap links

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'mb-izzo.github.io',
+  domain: 'mb-izzo.github.io',
   title: 'Izzo',
   subtitle: 'A blog for my thoughts',
   lang: 'en-US',
